6ef575272d781f086ee2bffae50759124ee72464
alicedigitallabs <32162302+AliceDigitalLabs@users.noreply.github.com>
2020-01-29 18:05:25 +0000

    scans for local BT LE devices gets MAC addresses. Interrim checkin!

--- little_list/code/little_list/www/js/app_specific/events/events.service.js
+++ little_list/code/little_list/www/js/app_specific/events/events.service.js
@@ -20,61 +20,84 @@
         var service = {
             
         };
-
-        var PAUSE_FOR_A_WHILE_MS = 3000;
-        var NUM_DUMMY_EVENTS = 10;
-
-
-        var createEvent = function(name, date, postcode){
-
-            var result = {
-                name : name,
-                date : date,
-                postcode: postcode
-            }
-            return result;
-        }
-
-        var createDummyEvents = function(numToCreate){
-            var result = [];
-
-            for(var index=0; index < numToCreate; index++){
-
-                var name = "event " + index;
-                var date = moment().add('years', index).toDate();
-                var postcode = "M1 5GD";
-
-                result.push(createEvent("event " + index, date, postcode));
-            }
-            return result;
-        }
+        var isScanning = false;
+        var TIMEOUT_MS = 10000;
 
 
-        var replaceWithRealCode = function(){
+        function startScan(){
             var deferred = $q.defer();
-            
+            isScanning = true;
+
             $timeout(
                 function(){
-                    eventsArray = createDummyEvents(NUM_DUMMY_EVENTS);
-                    deferred.resolve(eventsArray);
+                    stopScan();
+                    deferred.resolve();
+                }
+            ,TIMEOUT_MS);
+            
+            ble.startScan(
+                [],
+                function(device){
+                    eventsArray.push({
+                        id: device.id,
+                        name: device.name
+                    });
                 },
-            PAUSE_FOR_A_WHILE_MS);            
-            
-            
+                function(error){
+                    console.log("BLE: error " + JSON.stringify(error,null,2) );
+                    deferred.reject(error);
+                    
+                }
+            );
+
             return deferred.promise;
         }
 
-        var promiseToUpdateEvents = function(){
-            // returns a promise
-            return replaceWithRealCode();
+        function stopScan(){
+            if(isScanning){
+                isScanning = false;
+                ble.stopScan();
+            }
+
         }
 
+        function asyncScan(){
+            var deferred = $q.defer();
+
+            if(!isScanning){
+                startScan().then(
+                    function(){
+                        deferred.resolve();
+                    },
+                    function(){
+                        deferred.reject();
+                    }
+                );
+            }else{
+                $timeout(function(){
+                    deferred.resolve();
+                });
+            }
+
+            return deferred.promise;
+        }
+
+
         service.updateEvents = function(){
-            return promiseToUpdateEvents();   
+            return asyncScan(); 
         } 
 
+        function getNames(eventsArray){
+            var result = [];
+            eventsArray.forEach(function (event){
+                result.push(event.id);
+            });
+            return result;
+        }
+
+
         service.getEvents = function(){
-            return angular.copy(eventsArray);
+            return getNames(eventsArray);
         }
 
         service.getNumEvents = function(){
